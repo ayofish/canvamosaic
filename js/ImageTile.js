1a19c787aee9733758b60686d2aa6804a3dba8b5
Nestor Gimenez <ngimenez@spinifexit.com>
2016-09-14 18:20:21 +0800

    Added the Initial ImageTile class 	new file:   js/ImageTile.js

---
+++ js/ImageTile.js
@@ -0,0 +1,15 @@
+var ImageTile = function() {
+    this.r = 0;
+    this.g = 0;
+    this.b = 0;
+    return this;
+};
+
+ImageTile.prototype = {
+    getAverageColorRGB: function() {
+
+    },
+    getAverageColorHex: function() {
+
+    }
+};
